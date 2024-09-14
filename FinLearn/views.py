e216316c0184ce7175dcfdf7feaeb57feaf9760b
DeepeshP04 <pratapdeepesh4@gmail.com>
2024-09-14 23:59:35 +0530

    complete savings and investment main pages

--- FinLearn/views.py
+++ FinLearn/views.py
@@ -5,3 +5,11 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def index():
     return render_template("home.html")
+
+@views.route("/savings")
+def get_savings_tips():
+    return render_template("savings.html")
+
+@views.route("/investment")
+def get_investment():
+    return render_template("investment.html")
